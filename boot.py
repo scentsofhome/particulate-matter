0a7ccdbb061e6d9e1996d10b705e5e47c158e011
scentsofhome <scentsofhome4@gmail.com>
2026-07-06 16:45:36 -0400

    Update comments for clarity in boot.py

--- boot.py
+++ boot.py
@@ -1,4 +1,5 @@
-# Runs on boot. 
+### boot.py runs on the ESP32 on boot. 
+
 import network
 import time
 import webrepl
@@ -10,22 +11,22 @@ wlan_sta = network.WLAN(network.STA_IF)
 wlan_sta.active(False)
 time.sleep(0.5)
 
-# 2. Enable the Access Point (AP) interface
+# 2. Enable the Access Point (AP) interface.
 wlan_ap = network.WLAN(network.AP_IF)
 wlan_ap.active(True)
 
-# 3. Configure local network name and password
-AP_SSID = "ESP32-Sensor-Network"
-AP_PASSWORD = "tame"
+# 3. Configure local network name and password.
+AP_SSID = "PM Monitor"
+AP_PASSWORD = "humidity_conditioning"
 
 wlan_ap.config(essid=AP_SSID, password=AP_PASSWORD)
 
-# Give the hardware a brief moment to initialize the network radio
+# Give the hardware a moment to initialize the network.
 time.sleep(1)
 
-# Confirm network details in the terminal
+# Confirm network details in the terminal.
 print(f"Network '{AP_SSID}' is now broadcasting!")
-print("Board IP Address:", wlan_ap.ifconfig()[0]) # This will be 192.168.4.1
+print("Board IP Address:", wlan_ap.ifconfig()[0]) # This should be 192.168.4.1
 
-# 4. Start the webREPL
+# 4. Start the webREPL.
 webrepl.start()
